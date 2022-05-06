bd964fb082ea18c4ea57353fe1a02292e259c8be
tymoteuszjozwiak <tymoteusz.jozwiak@getgoogleoff.me>
2022-05-06 16:24:12 +0200

    fix getting config values & error reporting

--- backend/config/functions.php
+++ backend/config/functions.php
@@ -1,11 +1,17 @@
 <?php
 require('config.php');
 
+if($config['production'] !== true){
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+
 header('X-Frame-Options: sameorigin');
 header('X-XSS-Protection: 1; mode=block');
 header('X-Content-Type-Options: nosniff');
 
-$db = new mysqli($config->db->host, $config->db->user, $config->db->pass, $config->db->name);
+$db = new mysqli($config['db']['host'], $config['db']['user'], $config['db']['pass'], $config['db']['name']);
 $db->set_charset('utf8');
 
 function apiReturn($status_code, $response){
