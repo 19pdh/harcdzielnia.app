0d835491c6aa691b2ca5724ec2bacaf1950c306b
tymoteuszjozwiak <tymoteusz.jozwiak@getgoogleoff.me>
2022-05-06 16:25:16 +0200

    feat: items list endpoint

---
+++ backend/routes/items.php
@@ -0,0 +1,24 @@
+<?php
+$req_method = "GET";
+require('../includes/header.php');
+
+$sql = "SELECT id,name,image_url FROM items WHERE status=1";
+
+    $result = $db->query($sql);
+    if($result->num_rows == 0){
+        $items = 0;
+    } else {
+        $items = array();
+        while($row = $result->fetch_assoc()){
+            $items[$row['id']]['name'] = $row['name'];
+            $items[$row['id']]['image_url'] = $row['image_url'];
+        }
+    }
+
+$status_code = 200;
+$response = [
+    'items' => $items
+];
+
+apiReturn($status_code, $response);
+?>
